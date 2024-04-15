7c53c99c396cbad49a065314985c76e2a79e6ed1
Jensooon <jenson.wintle@gmail.com>
2024-04-15 18:31:07 +0100

    Added styling to catalog component

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css',
 })
-export class CatalogComponent {}
+export class CatalogComponent {
+  product: IProduct;
+
+  constructor() {
+    this.product = {
+      id: 2,
+      name: 'Friendly Bot',
+      description:
+        'A friendly robot head with two eyes and a smile -- great for dosmetic use.',
+      imageName: 'head-friendly.png',
+      category: 'Heads',
+      price: 945.0,
+      discount: 0.2,
+    };
+  }
+}
