159f73d342800e98950619e108264c8d22677c2d
Jensooon <jenson.wintle@gmail.com>
2024-04-15 16:13:30 +0100

    Completed section 3 of tutorial.

---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-site-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './site-header.component.html',
+  styleUrl: './site-header.component.css',
+})
+export class SiteHeaderComponent {}
