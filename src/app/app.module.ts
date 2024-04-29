b11f54079f40ecbcbf9e03f6da3fe4ee2a84159e
Jensooon <jenson.wintle@gmail.com>
2024-04-29 21:02:15 +0100

    Added basic page navigation to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,9 @@ import { HttpClientModule } from '@angular/common/http';
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
